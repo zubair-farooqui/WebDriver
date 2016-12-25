d32d07ef053d785d76b86d7ed03375eeb30304e6
Zubair <zubairufarooqui@hotmail.com>
2016-12-26 00:51:20 +0500

    WebDriver POM NUnit Example

---
+++ cSeleniumSharp/PageObjects/Google.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSeleniumSharp.PageObjects
+{
+    class Google
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Name, Using = "q")]
+        //[CacheLookup]
+        public IWebElement QuerySerch { get; set; }
+
+        public Google(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Search(String SearchText)
+        {
+            QuerySerch.Clear();
+            QuerySerch.SendKeys(SearchText);
+            QuerySerch.Submit();
+        }
+    }
+}
---
+++ cSeleniumSharp/TestCases/GoogleTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using OpenQA.Selenium;
+// Requires reference to WebDriver.Support.dll
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using cSeleniumSharp.PageObjects;
+
+using NUnit.Framework;
+
+namespace cSeleniumSharp.TestCase
+{
+    [TestFixture]
+    class GoogleTests
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com/");
+        }
+
+        [Test]
+        public void TestCheese()
+        {
+
+            Google google = new Google(driver);
+            google.Search("Cheese");
+
+            // Google's search is rendered dynamically with JavaScript.
+            // Wait for the page to load, timeout after 10 seconds
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+            // Should see: "Cheese - Google Search" (for an English locale)
+            Console.WriteLine("Page title is: " + driver.Title);
+
+            Assert.That("Cheese - Google Search", Is.EqualTo(driver.Title));
+            Assert.AreEqual("Cheese - Google Search", driver.Title);
+        }
+
+        [Test]
+        public void TestBread()
+        {
+
+            Google google = new Google(driver);
+            google.Search("Bread");
+
+            // Google's search is rendered dynamically with JavaScript.
+            // Wait for the page to load, timeout after 10 seconds
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.StartsWith("Bread", StringComparison.OrdinalIgnoreCase));
+
+            // Should see: "Bread - Google Search" (for an English locale)
+            Console.WriteLine("Page title is: " + driver.Title);
+
+            Assert.That("Bread - Google Search", Is.EqualTo(driver.Title));
+            Assert.AreEqual("Bread - Google Search", driver.Title);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Dispose();
+        }
+    }
+}
